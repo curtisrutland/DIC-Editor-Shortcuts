f0cc01f0a6a507e6adb16a91703d84f395cf6ef1
curtisrutland <curtisrutland@gmail.com>
2013-09-03 13:34:15 -0500

    added image files, updated manifest, fixed tabbing image files are icons. manifest references icons. background.js had incorrect tabbing

--- js/background.js
+++ js/background.js
@@ -1,13 +1,13 @@
 chrome.runtime.onMessage.addListener(
-        function(msg, sender, sendResponse) {
-            if (msg.text === "urlBinding")
-            {
-                $("#clipboard").val('');
-                $("#clipboard").focus();
-                document.execCommand('paste');
-                var contents = $("#clipboard").val();
-                if(contents.match("^http://"))
-                    sendResponse({val: contents});
-            }
-        }
+	function(msg, sender, sendResponse) {
+		if (msg.text === "urlBinding")
+		{
+			$("#clipboard").val('');
+			$("#clipboard").focus();
+			document.execCommand('paste');
+			var contents = $("#clipboard").val();
+			if(contents.match("^http://"))
+				sendResponse({val: contents});
+		}
+	}
 );
